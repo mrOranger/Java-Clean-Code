8a96bd3fc6b381a273094aa712e6572620e15e83
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-07-19 15:12:06 +0200

    Modified Main class with Anonymous class.

--- Thread/src/examples/concurrency/callable_future/Main.java
+++ Thread/src/examples/concurrency/callable_future/Main.java
@@ -1,5 +1,9 @@
 package examples.concurrency.callable_future;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,6 +23,36 @@ public class Main {
 		Future<Data> data = newThread.submit(new LeggiFile());
 		try {
 			System.out.println(data.get());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		/*
+		 * Siccome callable è un'interfaccia funzionale
+		 * possiamo anche passare una classe anonima, che rende tutto molto
+		 * simile a quanto accade per i Listerner in Android.
+		 */
+		newThread = Executors.newSingleThreadExecutor();
+		Future<String> newData = newThread.submit(new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+				var città = 0;
+				var totale = 0;
+				BufferedReader input = new BufferedReader(new FileReader(new File("data.dat")));
+				String line;
+				while((line = input.readLine()) != null) {
+					città++;
+					int tot = Integer.parseInt(line.split(":")[1]);
+					totale += tot;
+				}
+				input.close();
+				return "Citta: " + città + " - Totale: " + totale;
+			}
+		});
+		
+		try {
+			System.out.println(newData.get());
 		} catch (InterruptedException | ExecutionException e) {
 			e.printStackTrace();
 		}
