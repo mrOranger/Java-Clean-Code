4e16b573c28ec12490ee6466a78ee020b57216c9
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-07-20 10:59:18 +0200

    Created basic classes for final example.

--- Thread/src/examples/concurrency/order_manager/GestoreOrdini.java
+++ Thread/src/examples/concurrency/order_manager/GestoreOrdini.java
@@ -1,5 +1,48 @@
 package examples.concurrency.order_manager;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
+
 public class GestoreOrdini {
+	
+	private ConcurrentHashMap<String, List<Ordine>> mappaOrdini;
+	private LongAdder numeroOrdini;
+	
+	public GestoreOrdini() {
+		this.mappaOrdini = new ConcurrentHashMap<>();
+		this.numeroOrdini = new LongAdder();
+	}
+	
+	public void aggiungiOrdine(Ordine nuovoOrdine) {
+		/* 
+		 * Distinguiamo tra ordine già presente nella mappa
+		 * ed un ordine nuovo da aggiungere.
+		 */
+		if(!this.mappaOrdini.contains(nuovoOrdine.getId())) {
+			List<Ordine> nuoviOrdini = new ArrayList<>();
+			nuoviOrdini.add(nuovoOrdine);
+			this.mappaOrdini.put(nuovoOrdine.getId(), nuoviOrdini);
+		} else {
+			this.mappaOrdini.get(nuovoOrdine.getId()).add(nuovoOrdine);
+		}
+	}
+	
+	public void stampaOrdini() {
+		this.mappaOrdini.entrySet().stream().forEach(e -> {
+			System.out.println("Venditore: " + e.getKey());
+			e.getValue().forEach(System.out::println);
+			System.out.println();
+		});
+	}
+	
+	public void incrementaOrdine() {
+		this.numeroOrdini.increment();
+	}
+	
+	public long ordiniElaborati() {
+		return this.numeroOrdini.longValue();
+	}
 
 }
--- Thread/src/examples/concurrency/order_manager/Ordine.java
+++ Thread/src/examples/concurrency/order_manager/Ordine.java
@@ -1,5 +1,33 @@
 package examples.concurrency.order_manager;
 
 public class Ordine {
+	
+	private String id;
+	private double prezzo;
+	
+	public Ordine(String id, double prezzo) {
+		this.id = id;
+		this.prezzo = prezzo;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public double getPrezzo() {
+		return prezzo;
+	}
+	
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
 
+	@Override
+	public String toString() {
+		return "ID: " + this.getId() + " - Prezzo: " + this.getPrezzo();
+	}
 }
